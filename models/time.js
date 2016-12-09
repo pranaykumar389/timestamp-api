91b354fd338d21b1fdadcd698b4978dd1d2c1dbd
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2016-12-09 13:21:05 -0500

    Added get function

--- models/time.js
+++ models/time.js
@@ -10,6 +10,24 @@ var time = {
         res.sendFile(process.cwd() + '/views/index.html');
     },
 
+    get: function(req, res) {
+        var id = req.params.id,
+        timestamp = new Timestamp({});
+        
+        if(+id >= 0) {
+            timestamp.unix = +id;
+            timestamp.natural = moment.unix(timestamp.unix).format('MMMM D, YYYY');
+        }
+        if(isNaN(+id) && moment(id, 'MMMM D, YYYY').isValid()) {
+            timestamp.unix = +moment(id, 'MMMM D, YYYY').format('X');
+            console.log('timestamp.unix: ', timestamp.unix);
+            timestamp.natural = moment.unix(timestamp.unix).format('MMMM D, YYYY');
+            console.log('timestamp.natural: ', timestamp.natural);
+        }
+        res.json(timestamp);
+
+    }
+
 };
 
 module.exports = time;
