86de183391ffea210b23741e8c36a9444a9419ca
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-01-07 18:23:39 -0500

    modified timejs file

--- models/time.js
+++ models/time.js
@@ -2,7 +2,6 @@
 var mongoose = require('mongoose'),
     moment = require('moment'),
     Timestamp = mongoose.model('Timestamp');
-moment().format();
 
 var time = {
 
@@ -13,16 +12,22 @@ var time = {
     get: function(req, res) {
         var id = req.params.id,
         timestamp = new Timestamp({});
-        
-        if(+id >= 0) {
-            timestamp.unix = +id;
-            timestamp.natural = moment.unix(timestamp.unix).format('MMMM D, YYYY');
+
+        function unixToNatural(unix) {
+            return moment.unix(unix).format('MMMM D, YYYY');
         }
-        if(isNaN(+id) && moment(id, 'MMMM D, YYYY').isValid()) {
-            timestamp.unix = +moment(id, 'MMMM D, YYYY').format('X');
-            console.log('timestamp.unix: ', timestamp.unix);
-            timestamp.natural = moment.unix(timestamp.unix).format('MMMM D, YYYY');
-            console.log('timestamp.natural: ', timestamp.natural);
+        function naturalToUnix(id) {
+            return moment(id, 'MMMM D, YYYY').format('X');
+        }
+        
+        if(parseInt(id) >= 0) {
+            timestamp.unix = parseInt(id);
+            timestamp.natural = unixToNatural(timestamp.unix);
+        }
+        if(isNaN(parseInt(id)) && moment(id, 'MMMM D, YYYY').isValid()) {
+            timestamp.unix = parseInt(naturalToUnix(id)); 
+            timestamp.natural = unixToNatural(timestamp.unix); 
+        
         }
         res.json(timestamp);
 
